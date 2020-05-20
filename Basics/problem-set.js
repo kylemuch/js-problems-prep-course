c9873e5e659005b8694bbbe45417f2da254bd376
kylemuch <kylemuch@gmail.com>
2020-05-20 10:46:31 -0700

    Basics Exercises Completed

---
+++ Basics/problem-set.js
@@ -0,0 +1,58 @@
+//1.
+// console.log('Kyle ' + 'Much')
+
+//2.
+// let number = 4936
+// let ones = 4936 % 10
+// number -= ones
+// number /= 10
+// console.log(ones)
+
+// let tens = number % 10
+// number -= tens
+// number /= 10
+// console.log(tens)
+
+// let hundreds = number % 10
+// number -= hundreds
+// number /= 10
+// console.log(hundreds)
+
+// let thousands = number % 10
+// console.log(thousands)
+
+//3.
+// 'true' = string
+// false = boolean
+// 1.5 = number
+// undefined = undefined
+// {foo: 'bar'} = object
+// console.log(typeof undefined)
+
+
+//4.
+// var foo => statement, returns undefined
+// foo = 5 => expression, returns 5
+// foo => expression, returns 5
+
+//5.
+//console.log('5' + 10) implicitly converts 10 to a string, that why it returns '510', any
+// + expression that has a string operand produces a string result no matter what the other
+// operand is.
+
+//6.
+//console.log(Number('5') + 10)
+
+//7.
+// let myString = `The value of ${Number(5)} + 10 is 15`
+// console.log(myString)
+
+
+//8.
+// var foo = [1,2,3]
+// console.log(foo[3])
+// No, trying to access an index that is empty returns undefined, not an error.
+
+//9.
+// let names = ['milo', 'otis', 'apollo', 'butterscotch']
+
---
+++ ch-1/preparations_exercises/foo.js
@@ -0,0 +1,24 @@
+var foo = 'bar';
+console.log(foo);
+foo;
+var foo = 'bar';
+console.log(foo);
+foo;
+
+//5.
+// substring is a prototype method, constructor String
+// create is a static method, constructor Object
+// fromCharCode is a static method, constructor String
+// slice is a prototype method, constructor String, Array
+
+//6
+// index, lazyDog, operand2
+
+//7
+// index, lazyDog, operand2, CatName (could be a constructor)
+
+//8
+// BIG_NUMBER
+
+//9
+// snake_case, quick_Fox, 1stCharacter, 
